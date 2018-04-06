71424332cead2f0dcf68840d49c99e587f7df69f
zeflg <zw6aw@virginia.edu>
2018-04-05 22:05:12 -0400

    Finish coding 2

--- src/solution.java
+++ src/solution.java
@@ -32,7 +32,73 @@ public class solution {
         return sb.toString().substring(0, sb.length() - 1);
     }
 
+    public static String code2(String input) {
+        if (input.length() == 0) {
+            return input;
+        }
+        String[] words = input.split("\\|");
+        /**pre-process using the map-reduce function*/
+        String[] processed = new String[words.length];
+        int index = 0;
+        for (String origin : words) {
+            processed[index++] = mapreduce(origin);
+            /**compare using the map reduced version, also make sure the earier version is contained*/
+            for (int i = 0; i < index - 1; i++) {
+                if (processed[i] == null) {
+                    continue;
+                }
+                /**if they are equal*/
+                if (processed[index - 1].equals(processed[i])) {
+                    if (words[index - 1].length() < words[i].length()) {
+                        words[i] = null;
+                        processed[i] = null;
+                    } else {
+                        words[index - 1] = null;
+                        processed[index - 1] = null;
+                    }
+                    continue;
+                }
+                /**if contained, then remove contained one according to rules*/
+                if (processed[i].contains(processed[index - 1])) {
+                    words[index - 1] = null;
+                    processed[index - 1] = null;
+                } else if (processed[index - 1].contains(processed[i])) {
+                    words[i] = null;
+                    processed[i] = null;
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String s : words) {
+            if (s != null) {
+                sb.append(s + "|");
+            }
+        }
+        return sb.toString().substring(0, sb.length() - 1);
+    }
+
+    private static String mapreduce(String input) {
+        /**reduce leading, tailing and extra white space*/
+        input = input.trim().replaceAll("\\s+", " ");
+        String[] array = input.split(" ");
+        StringBuilder sb = new StringBuilder();
+        /**remove nonletter and nondigit*/
+        for (int i = 0; i < array.length; i++) {
+            char[] temp = array[i].toCharArray();
+            String s = "";
+            for (char c : temp) {
+                if (Character.isLetterOrDigit(c)) {
+                    s += Character.toLowerCase(c);
+                }
+            }
+            sb.append(s);
+            sb.append(" ");
+        }
+        return sb.toString().substring(0, sb.length() - 1);
+    }
+
     public static void main(String[] args) {
-        System.out.println(code1("20 3 4"));
+//        System.out.println(code1("20 3 4"));
+        System.out.println(code2("IBM cognitive computing|IBM \"cognitive\" computing is a revolution|  ibm cognitive  computing|'IBM Cognitive Computing' is a revolution?"));
     }
 }
