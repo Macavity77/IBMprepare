9d3fef38fadcf6e1ed04fc39374113c81e6d1d18
zeflg <zw6aw@virginia.edu>
2018-04-05 20:59:05 -0400

    Finish coding 1

--- src/solution.java
+++ src/solution.java
@@ -1,3 +1,38 @@
 public class solution {
 
+    public static String code1(String source) {
+        String[] info = source.split(" ");
+        int N = Integer.valueOf(info[0]);
+        int p = Integer.valueOf(info[1]);
+        int q = Integer.valueOf(info[2]);
+        /**check false input*/
+        if (N < 1 || p < 1 || q < 1 || q == p) {
+            throw new IllegalArgumentException("please have the right input");
+        }
+        StringBuilder sb = new StringBuilder();
+        String pval = String.valueOf(p);
+        String qval = String.valueOf(q);
+        for (int i = 1; i <= N; i++) {
+            String temp = "";
+            /**requirement one*/
+            if (i % q == 0 || i % p == 0) {
+                temp = temp + "OUT";
+            }
+            String num = String.valueOf(i);
+            if (num.contains(pval) || num.contains(qval)) {
+                temp = temp + "THINK";
+            }
+            if (temp.length() == 0) {
+                sb.append(i);
+            } else {
+                sb.append(temp);
+            }
+            sb.append(",");
+        }
+        return sb.toString().substring(0, sb.length() - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(code1("20 3 4"));
+    }
 }
